9cdb35be7f152433d75dc19de8863236db3c0554
luis conde <luisesnayra@gmail.com>
2024-06-11 15:10:31 -0600

    Update controller Tests

--- src/test/java/com/talentreef/interviewquestions/takehome/controllers/WidgetControllerTests.java
+++ src/test/java/com/talentreef/interviewquestions/takehome/controllers/WidgetControllerTests.java
@@ -2,8 +2,11 @@ package com.talentreef.interviewquestions.takehome.controllers;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -18,12 +21,14 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.MediaType;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
+import java.util.Optional;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -51,13 +56,42 @@ public class WidgetControllerTests {
     when(widgetService.getAllWidgets()).thenReturn(allWidgets);
 
     MvcResult result = mockMvc.perform(get("/v1/widgets"))
-               .andExpect(status().isOk())
-               .andDo(print())
-               .andReturn();
+        .andExpect(status().isOk())
+        .andDo(print())
+        .andReturn();
 
     List<Widget> parsedResult = objectMapper.readValue(result.getResponse().getContentAsString(),
-        new TypeReference<List<Widget>>(){});
+        new TypeReference<List<Widget>>() {
+        });
     assertThat(parsedResult).isEqualTo(allWidgets);
+  }
+
+  @Test
+  public void when_getWidgetByName_expect_single_widget() throws Exception {
+    Widget widget = Widget.builder().name("Im Get Widget by name").build();
+
+    when(widgetService.getWidgetByName("Im Get Widget by name")).thenReturn(java.util.Optional.of(widget));
+
+    mockMvc.perform(get("/v1/widgets/{name}", "Im Get Widget by name"))
+        .andExpect(status().isOk());
+  }
+
+  @Test
+  public void when_updateWidget_expect_widget_updated() throws Exception {
+    Widget widgetToUpdate = Widget.builder().name("Im a widget waiting to be updated").build();
+
+    when(widgetService.updateWidget(widgetToUpdate)).thenReturn(widgetToUpdate);
+
+    mockMvc.perform(put("/v1/widgets")
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(objectMapper.writeValueAsString(widgetToUpdate)))
+        .andExpect(status().isOk());
+
+  }
+
+  @Test
+  public void when_deleteWidgetByName_expect_toBe_deleted() throws Exception {
+    mockMvc.perform(delete("/v1/widgets/{name}", "Delete this widget")).andExpect(status().isOk());
   }
 
 }
